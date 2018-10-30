eca75bf03fb3a8598df2bff05799ce157f61659f
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-10-30 10:55:45 +0100

    feat() Include greet method and this
    [FA-139]

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+/* global Vue*/
+
 const vueApp = new Vue({
   el: '#app',
   data: {
-    title: 'Becoming a Vue ninja'
+    title: 'Becoming a Vue ninja',
+    name: 'Alberto'
+  },
+  methods: {
+    greet({ time }) {
+      return `Hello and good ${time}, ${this.name}.`;
+    }
   }
 });
