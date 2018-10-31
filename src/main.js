51319956e77f9853be0bb90f2ef7dc28dfac342b
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-10-31 11:44:57 +0100

    feat() Include a filter in Vue in order to make body posts shorter
    [FA-139]

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-new Vue({
+// create global filter
+Vue.filter('snippet', val =>
+  !val || typeof(val) !== 'string' ? '' : val.slice(0, 50));
+
+const vueApp = new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
