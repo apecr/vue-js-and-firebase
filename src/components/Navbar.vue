e958ab83e8d6bb55788ae9465f77c81f1e7b3936
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-10-31 16:12:35 +0100

    feat() Include several profile links
    [FA-139]

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
   <nav class="main-nav">
     <ul>
-      <li>
-        <router-link :to="{name: 'Home'}">Home</router-link>
-        <router-link :to="{name: 'About'}">About</router-link>
-      </li>
+      <li><router-link :to="{name: 'Home'}">Home</router-link></li>
+      <li><router-link :to="{name: 'About'}">About</router-link></li>
+    </ul>
+    <h2>User Profiles</h2>
+    <ul>
+        <li v-for="(user, index) in users" :key="index">
+            <router-link :to="{name: 'ViewProfile', params: {user_id: user.user_id}}">
+                <span>Profile {{ user.user_id }}</span>
+            </router-link>
+        </li>
     </ul>
   </nav>    
 </template>
@@ -13,7 +19,9 @@
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      users: [{ user_id: 1 }, { user_id: 2 }, { user_id: 3 }, { user_id: 4 }]
+    };
   }
 };
 </script>
