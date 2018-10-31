cd632b39882fc3376de8f1862636bb58837f18f7
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-10-31 12:11:37 +0100

    feat() Include a computed properties filtering the posts
    [FA-139]

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="blog">
         <h2>Blogs</h2>
-        <div v-for="post in posts" :key="post.id">
+        <input type="text" v-model="searchTerm">
+        <div v-for="post in filteredPosts" :key="post.id">
             <h3>{{post.title}}</h3>
             <p>{{post.body | snippet}}</p>
         </div>
@@ -15,8 +16,15 @@ export default {
   name: "Blogs",
   data() {
     return {
-      posts: []
+      posts: [],
+      searchTerm: ""
     };
+  },
+  methods: {},
+  computed: {
+    filteredPosts() {
+        return this.posts.filter(post => post.title.match(this.searchTerm))
+    }
   },
   created() {
     axios
