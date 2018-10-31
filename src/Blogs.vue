6a985e640298ba8707f38a97d2910755eedd22c7
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-10-31 11:17:25 +0100

    feat() Use life-cycle hooks in a Blogs component
    [FA-139]

---
+++ src/Blogs.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="blog">
+        <h2>{{ blogTitle }}</h2>
+        <button @click=changeTitle>Change Title</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogTitle: "Blogs"
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.blogTitle = "Amazing Blog Site";
+    }
+  },
+  beforeCreate() {
+    console.log("beforeCreate hook");
+  },
+  created() {
+    console.log("created hook");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate Hook");
+  }
+};
+</script>
